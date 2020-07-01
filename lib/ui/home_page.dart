18d16dff64f2cd39b6d564d605986f9bacdaaa53
muhammadarif <muhammad.arif5995@gmail.com>
2020-07-01 12:11:47 +0700

    assets

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,1 +1,60 @@
 import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Color(0xFF17ead9), Colors.lightBlueAccent],
+            ),
+          ),
+        ),
+        title: Text(
+          "Flights Result",
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontFamily: 'Banana',
+            fontSize: 25.0,
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        leading: GestureDetector(
+          onTap: null,
+          child: Icon(
+            Icons.arrow_back_ios,
+            color: Colors.white,
+          ),
+        ),
+      ),
+      body: LayoutBody(),
+    );
+  }
+}
+
+class LayoutBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Stack(
+        children: [
+          Column(
+            children: <Widget>[
+              Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height / 3.5,
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        colors: [Color(0xFF17ead9), Colors.lightBlueAccent])),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
