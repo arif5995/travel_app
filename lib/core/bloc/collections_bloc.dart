b42ada41971c2e2b333f3455f3700e466feee145
muhammadarif <muhammad.arif5995@gmail.com>
2020-07-20 16:27:35 +0700

    geolocator

--- lib/core/bloc/collections_bloc.dart
+++ lib/core/bloc/collections_bloc.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:geolocator/geolocator.dart';
 import 'package:rxdart/rxdart.dart';
 import 'package:travelapp/core/model/collection/CollectionResponse.dart';
 import 'package:travelapp/core/repositories/collection_repo.dart';
@@ -19,6 +20,7 @@ class CollectionEvent{
 
 class CollectionBlocs {
   final CollectionRepo repository = CollectionRepo();
+
   //stream to map
   final BehaviorSubject<CollectionEvent> _eventControllerCollection = BehaviorSubject<CollectionEvent>();
   StreamSink<CollectionEvent> get eventSinkCollection => _eventControllerCollection.sink;
@@ -29,12 +31,15 @@ class CollectionBlocs {
   Stream<CollectionState> get stateStream => _stateControllerCollection.stream;
 
   void _mapEventToState(CollectionEvent event) async{
+    final _position = await Geolocator().getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
     List<CollectionElement> data = [];
     List<CollectionElement> data1 = [];
     data.clear();
     if (event.text == null){
       try{
-        data = await repository.getCollections();
+        data = await repository.getCollections(lat: _position.latitude, lon: _position.longitude );
+        print("lat : ${_position.latitude}");
+        print("lat : ${_position.longitude}");
         _stateSinkCollection.add(CollectionList(collection: data));
         print("data : $data");
       }catch(e){
@@ -42,7 +47,7 @@ class CollectionBlocs {
       }
     } else {
       try{
-        data = await repository.getCollections();
+        data = await repository.getCollections(lat: _position.latitude, lon: _position.longitude);
         data.forEach((element) {
           if (element.collection.title.contains(event.text)) {
             data1.add(element);
--- lib/core/repositories/collection_repo.dart
+++ lib/core/repositories/collection_repo.dart
@@ -1,6 +1,7 @@
 
 
 import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
 import 'package:travelapp/core/model/collection/CollectionResponse.dart';
 import 'dart:convert';
 
@@ -8,15 +9,16 @@ class CollectionRepo {
   Dio dio = Dio();
 
   // ignore: missing_return
-  Future<List<CollectionElement>> getCollections() async {
+  Future<List<CollectionElement>> getCollections({double lat, double lon}) async {
     try {
       final response = await dio.get(
-          "https://developers.zomato.com/api/v2.1/collections?lat=-6.229728&lon=106.6894312",
+          "https://developers.zomato.com/api/v2.1/collections?lat=$lat&lon=$lon",
+          //"https://developers.zomato.com/api/v2.1/collections?lat=-6.229728&lon=106.6894312",
           options: Options(
               headers: {
                 "user-key": "cfd175d769fd9f8f63071f7b75d77a8d"
               }
-          )
+          ),
       );
       print("masuk");
       print(response.statusCode);
