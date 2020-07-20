a5f17ed4000478a2f4b33fd12e2982e7fbfb4aa7
muhammadarif <muhammad.arif5995@gmail.com>
2020-07-20 14:47:08 +0700

    button text

--- lib/ui/widget/button_text.dart
+++ lib/ui/widget/button_text.dart
@@ -3,33 +3,35 @@ import 'package:flutter/material.dart';
 
 Widget ButtonText({String text, IconData icon, GestureTapCallback onPress}) {
   return Container(
-    padding: EdgeInsets.all(10),
+    padding: EdgeInsets.all(5),
     child: RawMaterialButton(
       onPressed: onPress,
       splashColor: Colors.grey,
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(18.0),
       ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          Icon(
-            icon,
-            color: Colors.blue,
-            size: 20,
-          ),
-          SizedBox(
-            width: 5,
-          ),
-          Flexible(
-            child: Text(
-              text,
-              overflow: TextOverflow.ellipsis ,
-              style: TextStyle(color: Colors.blue, fontSize: 12),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Icon(
+              icon,
+              color: Colors.blue,
+              size: 20,
             ),
-          ),
-        ],
+            SizedBox(
+              width: 5,
+            ),
+            Flexible(
+              child: Text(
+                text,
+                overflow: TextOverflow.ellipsis ,
+                style: TextStyle(color: Colors.blue, fontSize: 12),
+              ),
+            ),
+          ],
+        ),
       ),
     ),
   );
