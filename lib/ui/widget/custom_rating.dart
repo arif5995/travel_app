5793730b702bac614d58aaf74da2e514ebf47c19
Cahyo Fendhi <cahyofendhi@gmail.com>
2020-07-27 14:33:43 +0700

    disable rating in platform web

--- lib/ui/widget/custom_rating.dart
+++ lib/ui/widget/custom_rating.dart
@@ -1,59 +1,56 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:travelapp/core/utils/currency_utils.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
 
 class CustomRating extends StatelessWidget {
-
   double rating;
   int review;
   bool useReview;
-  CustomRating({
-    @required this.rating,
-    this.review,
-    this.useReview = true
-  });
+  CustomRating({@required this.rating, this.review, this.useReview = true});
 
   @override
   Widget build(BuildContext context) {
     return Row(
       children: <Widget>[
-        RatingBar(
-          initialRating: rating,
-          minRating: 1,
-          direction: Axis.horizontal,
-          allowHalfRating: true,
-          itemCount: 5,
-          itemSize: 15,
-          itemBuilder: (context, _) => Icon(
-            Icons.star,
-            color: Colors.amber,
-          ),
-          onRatingUpdate: (rating) {
-            print(rating);
-          },
-        ),
-        SizedBox(width: 5),
+        !kIsWeb
+            ? RatingBar(
+                initialRating: rating,
+                minRating: 1,
+                direction: Axis.horizontal,
+                allowHalfRating: true,
+                itemCount: 5,
+                itemSize: 15,
+                itemBuilder: (context, _) => Icon(
+                  Icons.star,
+                  color: Colors.amber,
+                ),
+                onRatingUpdate: (rating) {
+                  print(rating);
+                },
+              )
+            : Container(),
+        SizedBox(width: kIsWeb ? 0 : 5),
         Text(
           rating.toString(),
           style: TextStyle(
-            color: Colors.black87,
-            fontWeight: FontWeight.bold,
-            fontSize: 12
-          ),
+              color: Colors.black87, fontWeight: FontWeight.bold, fontSize: 12),
         ),
-        useReview 
-          ? SizedBox(width: 5)
-          : SizedBox(),
+        // useReview
+        //   ? SizedBox(width: 5)
+        //   : SizedBox(),
 
-        useReview 
-          ? Text(
-            "(${formatter.format(review).toString()} Reviews)",
-            style: TextStyle(
-              color: Colors.black45,
-              fontSize: 12
-            ),
-          )
-          : SizedBox()
+        // useReview
+        //   ? Text(
+        //     "(${formatter.format(review).toString()} Reviews)",
+        //     style: TextStyle(
+        //       color: Colors.black45,
+        //       fontSize: 12
+        //     ),
+        //   )
+        //   : SizedBox()
       ],
     );
   }
