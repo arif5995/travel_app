b64fe6e6a0644db13f1d7ae55e995d8b143d6071
muhammadarif <muhammad.arif5995@gmail.com>
2020-08-04 11:05:43 +0700

    responsive

--- lib/ui/widget/item_list_citys.dart
+++ lib/ui/widget/item_list_citys.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-Container ItemListCitys({String itemImg, String itemTitle}){
+Container ItemListCitys({BuildContext context, String itemImg, String itemTitle}){
+  ScreenUtil.init(context);
   return Container(
-    height: 150,
+    height: 450.h,
     decoration: BoxDecoration(
       image: DecorationImage(
         image: NetworkImage(itemImg),
@@ -16,7 +18,7 @@ Container ItemListCitys({String itemImg, String itemTitle}){
       color: Colors.grey,
     ),
     child: Center(
-      child: Text(itemTitle, style: TextStyle(fontSize: 30, color: Colors.white70),),
+      child: Text(itemTitle, style: TextStyle(fontSize: ScreenUtil().setSp(40, allowFontScalingSelf: true), color: Colors.white70),),
     ),
   );
 }
